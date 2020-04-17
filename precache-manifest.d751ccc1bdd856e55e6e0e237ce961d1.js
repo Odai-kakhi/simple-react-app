e4cc5522c12228924609bed0158b8e3d716dea73
Uday Kakhi <uday.kakhi@booking.com>
2020-04-17 15:26:29 +0200

    Updates

---
+++ precache-manifest.d751ccc1bdd856e55e6e0e237ce961d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf71149229a473766edfe25fa535a70",
+    "url": "/simple-react-app/index.html"
+  },
+  {
+    "revision": "bb98539c8c6cf4896b9a",
+    "url": "/simple-react-app/static/css/main.f7fe1641.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/simple-react-app/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simple-react-app/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb98539c8c6cf4896b9a",
+    "url": "/simple-react-app/static/js/main.a18d1084.chunk.js"
+  },
+  {
+    "revision": "dcac7aa6e9cfc240f82d",
+    "url": "/simple-react-app/static/js/runtime-main.1c59b606.js"
+  }
+]);
